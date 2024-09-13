9423963f768bcb2dacaa6df6ec2fb33510ab0a13
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-09-13 09:30:48 -0400

    Fix issue with printing

--- CSC-402/Asistin_Blair_prog1.cpp
+++ CSC-402/Asistin_Blair_prog1.cpp
@@ -1,3 +1,6 @@
+//Name: Blair Asistin
+//CSC 402 Advanced Programming Methods
+//Program Description:
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,7 +8,7 @@
 #include <vector>
 using namespace std;
 
-//Header
+//Function Prototypes
 void processData(ifstream& inf, vector<int>& CPPStack, int* CStack, int size);
 void push(int*CStack, int size, int &numelts, int currentNumber);
 void pop(int*CStack, int &numelts);
@@ -24,16 +27,14 @@ int main() {
     cin >> inputFileName;
 
     inputFilePath += inputFileName;
-
     ifstream inf (inputFilePath);
-    inf.is_open();
 
     if(!inf) {
         cout << "Error opening file " << inputFilePath;
         return -1;
     }
 
-    //Gets the very first line of file which is the inputSize of CStack
+    //Gets the very first line of file which is the inputSize of CStack and store it in size variable.
     inf >> size;
 
     CStack = (int*)malloc(size * sizeof(int));
@@ -46,20 +47,21 @@ int main() {
     return 0;
 }
 
-void processData(ifstream& inf, vector<int>& CPPStack, int* CStack, int size) {
+//Determines what command to use and execute
+void processData(ifstream& inf, vector<int>& CPPStack, int* ptr_CStack, int size) {
 
-    int numelts = 0; //Keeps track the current index in CStack
+    int numelts = 0; //Keeps track of the current index in CStack
     string currentCommand;
     int currentNumber;
 
     //The while loop condition stores the command and number in separate variables
     while(inf >> currentCommand >> currentNumber) {
 
+        //Commands to be selected depending on the currentCommand variable
         if(currentCommand == "PUSH"){
+            CPPStack.push_back(currentNumber); //pushes an element in CPPStack
 
-            CPPStack.push_back(currentNumber);
-
-            push(CStack, size, numelts, currentNumber);
+            push(ptr_CStack, size, numelts, currentNumber); //pushes an element in CStack
         }
         else if(currentCommand == "POP") {
 
@@ -67,10 +69,10 @@ void processData(ifstream& inf, vector<int>& CPPStack, int* CStack, int size) {
                 cout << "NO POP - STACK IS EMPTY" << endl;
             }
             else {
-                CPPStack.pop_back();
+                CPPStack.pop_back();//pops an element in CPPStack
             }
 
-            pop(CStack, numelts);
+            pop(ptr_CStack, numelts); //pops an element in CStack
 
         }
         else if(currentCommand == "TOP") {
@@ -79,52 +81,73 @@ void processData(ifstream& inf, vector<int>& CPPStack, int* CStack, int size) {
                 cout << "NO TOP" << endl;
             }
             else {
-                cout << CPPStack[CPPStack.size()-1]<< endl;
+                cout << CPPStack.at(CPPStack.size()-1)<< endl;//Prints the top element of CPPStack
             }
 
-            top(CStack, numelts);
+            top(ptr_CStack, numelts);//Prints the top element of CStack
         }
     }
 
-    for(int i = 0; i < numelts; i++) {
-        cout << *(CStack + i) << endl;
+
+    cout << "CPPStack" << endl;
+
+    if(CPPStack.empty()) {
+        cout << "STACK IS EMPTY" << endl;
     }
+    else {
+        for(auto rit = CPPStack.rbegin(); rit != CPPStack.rend(); ++rit) {
+            cout << *rit << endl;
+        }
+    }
+
+    cout << "CSTACK" << endl;
+    if(numelts-1 == -1) {
+        cout << "STACK IS EMPTY";
+    }
+    else{
+
+        for(int i = numelts; i > 0; i--) {
+            cout << *(ptr_CStack + i) << endl;
+        }
+    }
+
 }
 
 //Push implementation for the CStack
-void push(int* CStack, int size, int &numelts, int currentNumber) {
+void push(int* ptr_CStack, int size, int &numelts, int currentNumber) {
 
     if(numelts < size) {
-        *(CStack+numelts) = currentNumber; //adding current size and CStack can obtain current index of CStack
+        *(ptr_CStack+numelts) = currentNumber; //adding current size and CStack can obtain current index of CStack
         numelts++;
     }
     else {
-        cout << "NO POP - STACK IS EMPTY" << endl;
+        cout << "NO PUSH - STACK IS EMPTY" << endl;
     }
 
 }
 
 //Pop implementation for CStack
-void pop(int* CStack, int &numelts) {
+void pop(int* ptr_CStack, int &numelts) {
 
-    if(numelts == 0) {
+    int temp = numelts;
+
+    if(temp-1 == -1) {
         cout << "NO POP - STACK IS EMPTY" << endl;
     }
     else {
-        CStack--; //By decrementing the CStack resulting to removing the last element to the stack.
-        numelts--;
+        ptr_CStack[numelts--];
     }
 
 }
 
 //Top implementation for the CStack
-void top(int*CStack, int numelts) {
+void top(int* ptr_CStack, int numelts) {
 
-    if(numelts == 0) {
+    if(numelts-1 == -1) {
         cout << "NO TOP" << endl;
     }
     else {
-        cout << *(CStack + numelts-1) << endl;
+        cout << *(ptr_CStack + numelts-1) << endl;
     }
 
 }
